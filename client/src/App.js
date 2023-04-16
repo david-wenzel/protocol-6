a83ab98172cc7ad271675c75a05cca30eba709f6
David Wenzel <dwenzel95@gmail.com>
2023-04-16 18:01:25 -0500

    areas boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,35 @@ import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import NavBar from "./NavBar";
 import Login from "./Login";
+import Areas from "./Areas";
 
 function App() {
+  const [areas, setAreas] = useState([]);
+
+  useEffect(() => {
+    fetch("/areas")
+      .then((res) => res.json())
+      .then((data) => {
+        setAreas(data);
+      });
+    console.log(areas);
+  }, []);
+
   return (
     <div className="App">
       <UserProvider>
         {/* <NavBar /> */}
         <Router>
           <Routes>
+            <Route
+              path="/areas"
+              element={
+                <Areas
+                  // errorsList={errorsList}
+                  areas={areas}
+                />
+              }
+            />
             <Route path="/login" element={<Login />} />
           </Routes>
         </Router>
---
+++ client/src/Areas.js
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import { UserContext } from "./context/user";
+import { NavLink } from "react-router-dom";
+import "./App.css";
+
+// import BoardCard from "./BoardCard";
+
+function Areas({ areas, errorsList }) {
+  const { loggedIn } = useContext(UserContext);
+
+  const renderAreas = areas.map((area) => (
+    <li key={area.id}>
+      <NavLink to={`/area/${area.id}`}>
+        <h1 id="section">{area.title}</h1>
+      </NavLink>
+    </li>
+  ));
+
+  if (loggedIn) {
+    return <div>{renderAreas}</div>;
+  } else {
+    return (
+      <div>
+        <h3>Please Login or Signup</h3>
+      </div>
+    );
+  }
+}
+
+export default Areas;
