db49b5a69e455b402f717eb26fea4a4f9d0e4db2
David Wenzel <dwenzel95@gmail.com>
2023-04-16 17:51:13 -0500

    update copy

--- client/src/Login.js
+++ client/src/Login.js
@@ -40,20 +40,7 @@ function Login() {
   return (
     <>
       <div>
-        <p className="about">
-          In a world where imagination knows no bounds, the beauty of
-          inspiration lies in sharing it with others. inpso.group provides a
-          unique platform for individuals to connect, explore and discover
-          inspiration from a global community. The app encourages users to share
-          their visions, creating a collective pool of ideas and creativity that
-          transcends boundaries. The power of a great idea is amplified by the
-          diverse perspectives and energy of inpso.group, enabling concepts to
-          take on a life of their own and evolve into something even greater
-          than their creators imagined. As we continue to explore and shape the
-          possibilities of the world around us, inpso.group remains a hub for
-          collaboration and inspiration, where individuals can collectively push
-          the boundaries of creativity and bring their dreams to life.
-        </p>
+        <p className="about">protocol 6</p>
         <form onSubmit={handleSubmit}>
           <span style={{ fontWeight: "bold" }}>Login:</span>
           <br />
