df76307db6ab6e26e9adb0ee3edb48bf5df6960e
David Wenzel <dwenzel95@gmail.com>
2023-04-16 11:13:59 -0500

    current user functions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,26 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  before_action :authorize
+
+  def current_user
+    @current_user ||= session[:user_id] &&
+    User.find_by(id: session[:user_id])
+  end
+
+  private 
+  
+  def authorize
+    current_user
+    return render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :user_id
+  end
+
+  def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+
+
 
 end
