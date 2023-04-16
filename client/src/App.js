dbe06497d8747211a443b88a3168327ab07c5120
David Wenzel <dwenzel95@gmail.com>
2023-04-16 16:39:29 -0500

    boiler plate app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,42 @@
 import { UserProvider } from "./context/user";
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <div className="App">
-      <UserProvider></UserProvider>
+      <UserProvider>
+        <NavBar />
+        <Router>
+          <Routes>
+            <Route
+              path="/areas"
+              element={
+                <Boards
+                  errorsList={errorsList}
+                  boards={boards}
+                  addBoard={addBoard}
+                />
+              }
+            />
+            <Route
+              path="/areas/:id"
+              element={
+                <Posts
+                  errorsList={errorsList}
+                  deletePost={deletePost}
+                  handleEditPost={handleEditPost}
+                  addPost={addPost}
+                  boards={boards}
+                />
+              }
+            />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Router>
+      </UserProvider>
     </div>
   );
 }
