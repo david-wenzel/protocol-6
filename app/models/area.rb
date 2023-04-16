7f94de2467d786919f78d4e3a4aba6f709b65aa8
David Wenzel <dwenzel95@gmail.com>
2023-04-16 11:21:56 -0500

    relationships set

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,2 +1,6 @@
 class Area < ApplicationRecord
+    has_many :protocols, dependent: :destroy
+    has_many :users, through: :protocols
+
+    validates :title, presence: true, uniqueness: true
 end
--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -1,2 +1,6 @@
 class Protocol < ApplicationRecord
+    belongs_to :user
+    belongs_to :area
+  
+    validates :img_url, :body, presence: true
 end
