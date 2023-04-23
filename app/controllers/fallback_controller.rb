0359364483f2fde925f4bc93220c7c24f061cbff
David Wenzel <dwenzel95@gmail.com>
2023-04-22 19:53:05 -0500

    remove files

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -1,14 +1,9 @@
 # Controller logic: fallback requests for React Router.
 # Leave this here to help deploy your app later!
-# class FallbackController < ActionController::Base
-
-#   def index
-#     # React app index page
-#     render file: 'public/index.html'
-#   end
-# end
 class FallbackController < ActionController::Base
+
   def index
-    render :index
+    # React app index page
+    render file: 'public/index.html'
   end
 end
